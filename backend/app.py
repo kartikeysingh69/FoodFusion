8b378a9696213830b3b2a025562d34f41505e9e8
Kartikey Singh <kunalsinghji94@gmail.com>
2026-07-19 23:20:19 +0530

    Added Docker Compose with Flask and MySQL

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@mysql:3306/foodfusion"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# 👇 Yahan model paste karna hai
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    message = db.Column(db.Text)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# 👇 Table create karne ke liye
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
