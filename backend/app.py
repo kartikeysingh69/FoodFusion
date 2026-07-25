34cd2ae02890eabb84081168485c2baddda48fa0
Kartikey Singh <kunalsinghji94@gmail.com>
2026-07-26 00:25:34 +0530

    Deploy FoodFusion Flask app on Kubernetes

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@mysql:3306/foodfusion"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@mysql-service:3306/foodfusion"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
